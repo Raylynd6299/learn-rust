81cb8a3ae6d86266c5cc790cacea4bce480c2292
Ray Pulido <ray_escom@outlook.com>
2024-12-09 19:57:04 -0600

    feat:  add array and vectors

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,21 @@
 fn main() {
-    let mut name: String = String::new(); // Cadena de texto, mnos optimo pero mas accesible
-    let mut age: String = String::new();
+    let mut names: Vec<String> = Vec::new();
 
-    println!("Introduce tu nombre: ");
-    //Recibir data del user
-    std::io::stdin().read_line(&mut name).unwrap();
-    name = name.trim().to_string();
 
-    println!("Introduce tu edad: ");
-    std::io::stdin().read_line(&mut age).unwrap();
+    for _index in 1..5 {
+        println!("Porfavor introduce un nombre: ");
 
-    //  convertir a num
-    let age_int: u8 = age.trim().parse().unwrap();
+        let mut name: String = String::new();
+        std::io::stdin().read_line(&mut name).unwrap();
 
-    println!("Hola Bienvenido {name} - {age_int}");
+        name = name.trim().to_string();
 
-    if age_int >= 18 && age_int <= 40 {
-        println!("Puedes entrar al bar");
-    } else if age_int > 40 {
-        println!("Eres muy viejo")
-    } else {
-        println!("ALTO!!! No puedes entrar al bar!")
+        names.push(name);
+    }
+
+    println!("{:?} {} ",names, names.len());
+
+    for internal_name in names{
+        println!("{internal_name}")
     }
 }
